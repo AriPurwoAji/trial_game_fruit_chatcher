7559e77c122b9584e87faaeabb79b544a09e84b2
AriPurwoAji <ajiaripurwo@gmail.com>
2026-02-17 23:32:09 +0700

    update

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -1,20 +1,45 @@
+import 'dart:async' as dart_async;
+import 'dart:math';
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:coba_game/services/audio_manager.dart';
+import 'fruit.dart';
+import 'basket.dart';
+import 'package:flame_audio/flame_audio.dart';
+
 
 class FruitCatcherGame extends FlameGame {
   @override
   Color backgroundColor() => const Color(0xFF87CEEB);
 
   final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+  late Basket basket;
+  final Random random = Random();
+
   void incrementScore() {
     scoreNotifier.value++;
+    AudioManager().playSfx('collect.mp3');
   }
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     // play background music
+    AudioManager().initialize();
     AudioManager().playBackgroundMusic();
+
+    basket = Basket();
+    add(basket);
+
+    _startSpawningFruit();
+  }
+
+  void _startSpawningFruit() {
+    dart_async.Timer.periodic(const Duration(seconds: 1), (timer) {
+      final xPosition = random.nextDouble() * size.x;
+
+      add(Fruit(position: Vector2(xPosition, -20)));
+    });
   }
 }

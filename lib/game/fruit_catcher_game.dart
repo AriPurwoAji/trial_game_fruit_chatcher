1d449fd46b308ab5654945964811ae929727b63c
AriPurwoAji <ajiaripurwo@gmail.com>
2026-02-17 21:54:04 +0700

    final  file fruit_chatcher.dart

--- lib/game/fruit_catcher_game.dart
+++ lib/game/fruit_catcher_game.dart
@@ -2,5 +2,12 @@ import 'package:flutter/widgets.dart';
 
 class FruitCatcherGame extends FlameGame {
   @override
-  Color backgroundColor() => const Color(0xFF87CEEB); // Sky blue background
+  Color backgroundColor() => const Color(0xFF87CEEB);
+
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+  }
 }
